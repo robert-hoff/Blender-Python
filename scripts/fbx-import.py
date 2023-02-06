513c35a759ee39357d73f7a7eb8a00a8aa5c35f3
Hoff Inc <hoff.rj@gmail.com>
2023-02-06 01:34:37 +0000

    full script

--- scripts/fbx-import.py
+++ scripts/fbx-import.py
@@ -1,26 +1,7 @@
 import bpy
 import os
-import sys
 
-# delete everything
-def deleteAllObjects():
-    deleteListObjects = ['MESH', 'CURVE', 'SURFACE', 'META', 'FONT', 'HAIR', 'POINTCLOUD', 'VOLUME', 'GPENCIL',
-                     'ARMATURE', 'LATTICE', 'EMPTY', 'LIGHT', 'LIGHT_PROBE', 'CAMERA', 'SPEAKER']
-    for o in bpy.context.scene.objects:
-        for i in deleteListObjects:
-            if o.type == i:
-                o.select_set(False)
-            else:
-                o.select_set(True)
-    bpy.ops.object.delete()
-
-
-
-importdir = "Z:\\dev\\unity3d\\Rock and Vegetation Pack\Assets\\Low Poly Modular Terrain Pack\\Terrain_Assets\\Meshes\\Terrain\\CPT\\NoLOD\\M"
-testdir = "Z:\\active\\projects\\edinburgh-gamejam\\blender"
-exportdir = "Z:\active\projects\edinburgh-gamejam\exportdir"
-
-
+# list directory files of given extension
 def listFiles(dir, ext):
     fileList = []
     for file in os.listdir(dir):
@@ -29,39 +10,32 @@ def listFiles(dir, ext):
     return fileList
 
 
-# files = listFiles(importdir,"fbx")
-# print(files[0])
-# print(files[1])
+# delete everything in the file
+def deleteAllObjects():
+    for some_object in bpy.data.objects:
+        bpy.data.objects.remove( some_object )
 
-singlefile = importdir+"\\CPT_Terrain_M_a_01.fbx"
+importdir = "Z:\\dev\\unity3d\\Rock and Vegetation Pack\Assets\\Low Poly Modular Terrain Pack\\Terrain_Assets\\Meshes\\Terrain\\CPT\\NoLOD\\M"
+exportdir = "Z:\\active\\projects\\edinburgh-gamejam\\exportdir\\"
 
-# deleteAllObjects()
-# bpy.ops.import_scene.fbx(filepath=singlefile)
+counter = 1
 
-# imports target fbx
-# bpy.ops.import_scene.fbx( filepath = "Z:\\dev\\unity3d\\Rock and Vegetation Pack\Assets\\Low Poly Modular Terrain Pack\\Terrain_Assets\\Meshes\\Terrain\\CPT\\NoLOD\\M\\CPT_Terrain_M_a_01.fbx")
-
-
-
-
-my_texture_path = "Z:\\github\\LSystemsMG\\LSystemsMG\\Content\\terrain-tiles\\terrain-texturemap.png"
-for node in bpy.data.objects[0].data.materials[0].node_tree.nodes:
-    if node.name == 'Image Texture':
-        node.image.filepath = my_texture_path
-    if node.name == 'Principled BSDF':
-        node.inputs['Alpha'].default_value = 1.0
-
-
-bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
-
-
-
-
-
-
-
-
-
+for filename in listFiles(importdir, 'fbx'):
+    filenamepath = importdir+"\\"+filename;
+    deleteAllObjects()
+    bpy.ops.import_scene.fbx( filepath = filenamepath)
+    my_texture_path = "Z:\\github\\LSystemsMG\\LSystemsMG\\Content\\terrain-tiles\\terrain-texturemap.png"
+    for node in bpy.data.objects[0].data.materials[0].node_tree.nodes:
+        if node.name == 'Image Texture':
+            node.image.filepath = my_texture_path
+        if node.name == 'Principled BSDF':
+            node.inputs['Alpha'].default_value = 1.0
+    bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
+    newname = "terrain"+str(counter)+".fbx"
+    # export scene operators:
+    # https://docs.blender.org/api/current/bpy.ops.export_scene.html
+    bpy.ops.export_scene.fbx(filepath=exportdir+newname, path_mode='STRIP', axis_up='Z')
+    counter = counter + 1
 
 
 
